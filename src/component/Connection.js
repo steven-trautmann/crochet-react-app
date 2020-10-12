441741f2774b46a5f79eefa32d5ec0319cba98b1
Isti <steven.trautmann1@gmail.com>
2020-10-12 20:38:36 +0200

    sending email to myself

--- src/component/Connection.js
+++ src/component/Connection.js
@@ -22,6 +22,49 @@ export default function Connection() {
         </ul>
       </div>
       <div>
+        <form class="gform pure-form pure-form-stacked" method="POST" data-email="example@email.net"
+          action="https://script.google.com/macros/s/AKfycbxOIeZLfLu1rAjdt0RzjUzA-eTfOcROJCKrzCBQ4vW-pLcZaA/exec">
+
+          <div class="form-elements">
+            <fieldset class="pure-group">
+              <label for="name">Name: </label>
+              <input id="name" name="name" placeholder="What your Mom calls you" />
+            </fieldset>
+
+            <fieldset class="pure-group">
+              <label for="message">Message: </label>
+              <textarea id="message" name="message" rows="10"
+                placeholder="Tell us what's on your mind..."></textarea>
+            </fieldset>
+
+            <fieldset class="pure-group">
+              <label for="email"><em>Your</em> Email Address:</label>
+              <input id="email" name="email" type="email"
+                required placeholder="your.name@email.com" />
+            </fieldset>
+
+            <fieldset class="pure-group">
+              <label for="color">Favourite Color: </label>
+              <input id="color" name="color" placeholder="green" />
+            </fieldset>
+
+            <fieldset class="pure-group honeypot-field">
+              <label for="honeypot">To help avoid spam, utilize a Honeypot technique with a hidden text field; must be empty to submit the form! Otherwise, we assume the user is a spam bot.</label>
+              <input id="honeypot" type="text" name="honeypot" value="" />
+            </fieldset>
+
+            <button class="button-success pure-button button-xlarge">
+              <i class="fa fa-paper-plane"></i>&nbsp;Send</button>
+          </div>
+
+          <div class="thankyou_message" style={{ display: "none" }}>
+            <h2><em>Thanks</em> for contacting us! We will get back to you soon!</h2>
+          </div>
+        </form>
+
+        <script data-cfasync="false" src="form-submission-handler.js"></script>
+      </div>
+      <div>
         <h2 style={{ margin: "2vw" }}>Közösségi Média:</h2>
       </div>
     </div>
