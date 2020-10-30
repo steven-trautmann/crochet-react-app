b2d8e37fc2a6284b8b13427f096adace141d4d4c
Isti <steven.trautmann1@gmail.com>
2020-10-30 10:22:52 +0100

    img on click increase

--- src/component/DisplayPicWithoutDetails.js
+++ src/component/DisplayPicWithoutDetails.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import "../style/links.css";
+import "../style/prevProductsImg.css"
 
 const Img = styled.img`
     &:hover {
@@ -9,14 +10,55 @@ const Img = styled.img`
   `;
 
 export default function DisplayPicWithoutDetails(props) {
+    const [itsIncreased, setIntsIncreased] = useState(false);
+    const [offsetTop, setOffsetTop] = useState(0);
+    const [countainerIdName, setCountainerIdName] = useState("");
+    const [idName, setIdName] = useState("");
+
+    const toggleClassName = () => {
+        if (itsIncreased) {
+            setOffsetTop(0);
+            setCountainerIdName("");
+            setIdName("");
+            enableScroll();
+            setIntsIncreased(false);
+        } else {
+            setOffsetTop(window.scrollY);
+            setCountainerIdName("prevProductsContainerIncreased");
+            setIdName("prevProductsIncreased");
+            disableScroll();
+            setIntsIncreased(true);
+        }
+    }
+
+
+    function disableScroll() {
+        // Get the current page scroll position 
+        let scrollTop = window.scrollY;
+        let scrollLeft = window.scrollX;
+
+        // if any scroll is attempted, set this to the previous value 
+        window.onscroll = function () {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+    }
+
+    function enableScroll() {
+        window.onscroll = function () { };
+    }
+
 
     return (
-        <div style={{ width: props.pictureSquareDistance }}>
-            <Img
-                style={{ height: `${props.pictureSquareDistance}`, width: `${props.pictureSquareDistance}` }}
-                src={props.imgSrc}
-                alt="crochetProduct"
-            />
+        <div id={countainerIdName}
+            style={{ top: itsIncreased ? offsetTop : null }}
+            onClick={() => { toggleClassName() }}>
+            <div id={idName} style={{ height: `${itsIncreased ? "100%" : props.pictureSquareDistance}`, width: itsIncreased ? "auto" : props.pictureSquareDistance }}>
+                <Img
+                    style={{ height: "100%", width: "100%" }}
+                    src={props.imgSrc}
+                    alt="crochetProduct"
+                />
+            </div>
         </div>
     );
 }
