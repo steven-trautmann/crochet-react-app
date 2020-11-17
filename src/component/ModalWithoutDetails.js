2cfc1c05e2dbe01dc47d2f1c25765b9c0242dfc6
Isti <steven.trautmann1@gmail.com>
2020-11-17 10:17:00 +0100

    modal without details adjusted for mobile

--- src/component/ModalWithoutDetails.js
+++ src/component/ModalWithoutDetails.js
@@ -6,11 +6,10 @@ import KeyDownHandler from "./KeyDownHandler";
 
 const ArrowButton = styled.button`
     background-color: white;
-    width: 5vw;
+    width: 10vw;
     height: 100vh;
     opacity: 90%;
     border: 0;
-    text-decoration: none; 
     font-family: 'Times New Roman', Times, serif;
     font-size: 1.5rem;
     &:focus{
@@ -93,13 +92,13 @@ export default function ModalWithoutDetails(props) {
                             <ArrowButton>X</ArrowButton>
                         }
                         <ImgSuspense
-                            style={{ height: "auto", width: "auto", maxWidth: "100vw", maxHeight: "100vh" }}
+                            style={{ height: "auto", width: "auto", maxWidth: "80vw", maxHeight: "100vh" }}
                             src={props.imgSrc}
                             alt="crochetProduct"
                             fallback={<img
                                 src="/specialImages/loading.gif"
                                 alt="loading"
-                                style={{ height: "auto", width: "auto", maxWidth: "100vw", maxHeight: "100vh", borderRadius: "20%" }}
+                                style={{ height: "auto", width: "auto", maxWidth: "80vw", maxHeight: "100vh", borderRadius: "20%" }}
                             ></img>}
                         />
                         {props.imgSrcList.indexOf(props.imgSrc) < props.imgSrcList.length - 1 ?
